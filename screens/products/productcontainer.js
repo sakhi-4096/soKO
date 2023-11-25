e93119708d7aecea76076a43f3707031d185feed
Sakhile L. Ndlazi <sakhilelindah@protonmail.com>
2023-11-26 01:44:28 +0200

    Add ProductContainer test usage.

---
+++ screens/products/productcontainer.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native'
+
+const ProdductContainer = () => {
+  return (
+    <View>
+      <Text>Product Container</Text>
+    </View>
+  )
+}
+
+export default ProdductContainer;
