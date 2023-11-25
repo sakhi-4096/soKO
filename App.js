f3acc2cf94e14b19be8dcb6652637caed6c24137
Sakhile L. Ndlazi <sakhilelindah@protonmail.com>
2023-11-26 01:43:48 +0200

    Modify the content of app through the Product Container.

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Screens
+import ProductContainer from './screens/products/productcontainer'
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ProductContainer />
     </View>
   );
 }
