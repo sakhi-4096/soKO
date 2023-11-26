162a126b7de974d8e92fcbafa3070087c96edcf4
Sakhile L. Ndlazi <sakhilelindah@protonmail.com>
2023-11-26 21:48:00 +0200

    Display item on the store.

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 // Screens
-import ProductContainer from './screens/products/productcontainer'
+import Header from './shared/header';
+import ProductContainer from './screens/products/container'
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header />
       <ProductContainer />
     </View>
   );
